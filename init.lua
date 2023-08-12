d31c929b7e81c76cb3af7551b7cb5e57b44711a1
nikpan1 <96376932+nikpan1@users.noreply.github.com>
2023-08-12 10:33:25 +0200

    based on nvchad (not minimalistic)

--- init.lua
+++ init.lua
@@ -1,11 +1,21 @@
-require("nikodem.plugins-setup")
-require("nikodem.core.options")
-require("nikodem.core.keymaps")
-require("nikodem.core.coloscheme")
-require("nikodem.plugins.comment")
-require("nikodem.plugins.nvim-tree")
-require("nikodem.plugins.telescope")
-require("nikodem.plugins.nvim-cmp")
-require("nikodem.plugins.lsp.mason")
-require("nikodem.plugins.lsp.lspconfig")
-require("nikodem.plugins.lsp.lspsaga")
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
